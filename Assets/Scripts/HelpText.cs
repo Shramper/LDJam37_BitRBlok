e915789f9946f5dcdf780b3b056a42b82ab3c080
Dave Tamayo <djbtamayo@gmail.com>
2016-12-10 02:12:40 -0500

    Put in some tutorial text Doors now move you to the opposite door instead of a random door

---
+++ Assets/Scripts/HelpText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class HelpText : MonoBehaviour {
+	
+	TextMesh textMesh;
+
+	void Awake () {
+
+		textMesh = this.GetComponent<TextMesh>();
+	}
+
+	void Update () {
+		
+		float newAlpha = textMesh.color.a;
+
+		newAlpha -= 0.15f * Time.deltaTime;
+		textMesh.color = new Color (1, 1, 1, newAlpha);
+
+		if(newAlpha <= 0) {
+
+			Destroy(this.gameObject);
+		}
+	}
+}
