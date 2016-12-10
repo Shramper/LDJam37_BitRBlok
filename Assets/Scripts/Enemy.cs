ff1ba605420e639c0f52d61046416e2271a4be65
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2016-12-10 15:47:50 -0500

    Have an enemy that follows the player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float minWaitTime;
+    [SerializeField] float maxWaitTime;
+    [SerializeField] float stopDistance;
+    [SerializeField] float speed;
+
+    Vector3 target;
+    bool foundPlayer = false;
+    bool move = false;
+
+	// Use this for initialization
+	void Start () {
+        FindPlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FindPlayer();
+        MoveToPlayer();
+	}
+
+    void FindPlayer() {
+        if (GameObject.FindGameObjectWithTag("Player") != null) {
+            foundPlayer = true;
+            target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform.localPosition;
+            move = true;
+            Debug.Log("Found Player At: " + target);
+        }
+    }
+
+    void MoveToPlayer() {
+        // Check to make sure you are within range of the player 
+        if (Vector3.Distance(this.transform.position, target) > stopDistance) {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
+        }
+    }
+
+    void MeleeAttack() {
+
+    }
+}
---
+++ Assets/Scripts/RebDoor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RebDoor : MonoBehaviour {
+
+	
+	[SerializeField] GameObject opposingDoor;
+	[SerializeField] BoxSpawner boxSpawner;
+	[SerializeField] Image overlay;
+	[SerializeField] float fadeSpeed = 0.1f;
+
+	bool fadingOut = false;
+
+	void Update () {
+
+		if (fadingOut) {
+
+			float overlayAlpha = overlay.color.a;
+			overlayAlpha -= fadeSpeed * Time.deltaTime;
+			overlay.color = new Color (0, 0, 0, overlayAlpha);
+
+			if (overlayAlpha <= 0) {
+
+				fadingOut = false;
+				overlay.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.CompareTag("Player")) {
+
+			overlay.gameObject.SetActive(true);
+			overlay.color = Color.black;
+			fadingOut = true;
+
+			other.transform.position = opposingDoor.transform.position + 1.5f * opposingDoor.transform.forward;
+
+			boxSpawner.SpawnBoxes();
+		}
+	}
+}
