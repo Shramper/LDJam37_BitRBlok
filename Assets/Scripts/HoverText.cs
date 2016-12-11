cdb89f310b9e3fb64d7a0f6e9800ffe7a11c0345
Dave Tamayo <djbtamayo@gmail.com>
2016-12-11 15:01:20 -0500

    Adding in music files, fonts. Cleaned up the title screen a bit. Created a prefab of the main game music audio source.

---
+++ Assets/Scripts/HoverText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class HoverText : MonoBehaviour {
+	
+	[SerializeField] float hoverSpeed = 1;
+
+	int direction = 1;
+	float timer = 0;
+
+
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if(timer >= 1) {
+
+			direction *= -1;
+			timer = 0;
+		}
+
+		this.transform.Translate(direction * hoverSpeed * Time.deltaTime * Vector3.up);
+	}
+}
